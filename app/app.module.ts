522cef4673284de567f68c44a5686cdd90752fc5
Elliott Beach <elliott2.71828@gmail.com>
2016-11-19 17:35:21 -0600

    add components/app directory

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent }  from './components/app.component';
+import { AppComponent }  from './components/app/app.component';
 
 @NgModule({
   imports:      [ BrowserModule, NgbModule.forRoot() ],
