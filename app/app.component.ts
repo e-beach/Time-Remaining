300b2da27ab68c4d636b023bad81c7d537e6d95f
Elliott Beach <elliott2.71828@gmail.com>
2016-11-19 15:05:28 -0600

    add basic template

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello Angular</h1>`
+    template: `
+    <div class="container">
+     <nav class="navbar navbar-light bg-faded">
+       <div class="container-fluid">
+         <div class="navbar-header">
+          <a href="#" class="navbar-brand"> Time Remaining</a>
+         </div>
+       </div>
+     </nav>
+   </div>
+   <div class="input data-provider=datepicker">
+   </div>
+   <ngb-panel title="panel">
+      This app needs two features:
+        <ol>
+          <li>User can add a deadline and give it a name</li>
+          <li>User can view time remaining, in weeks, for deadline</li>
+        </ol>
+   </ngb-panel>
+   `
 })
 export class AppComponent { }
