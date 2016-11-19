e3433d859b36e64d0b61f2530ac42028fcac1321
Elliott Beach <elliott2.71828@gmail.com>
2016-11-19 17:29:00 -0600

    move main page to own file

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,25 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `
-    <div class="container">
-     <nav class="navbar navbar-light bg-faded">
-       <div class="container-fluid">
-         <div class="navbar-header">
-          <a href="#" class="navbar-brand"> Time Remaining</a>
-         </div>
-       </div>
-     </nav>
-   </div>
-   <div class="input data-provider=datepicker">
-   </div>
-   <ngb-panel title="panel">
-      This app needs two features:
-        <ol>
-          <li>User can add a deadline and give it a name</li>
-          <li>User can view time remaining, in weeks, for deadline</li>
-        </ol>
-   </ngb-panel>
-   `
+    templateUrl: 'app/app.component.html'
 })
 export class AppComponent { }
