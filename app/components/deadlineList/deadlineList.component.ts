6caac861669f383c4b78fedcc8bb58d76d9769ab
Elliott Beach <elliott2.71828@gmail.com>
2016-11-20 14:55:49 -0600

    add deadlineList component

---
+++ app/components/deadlineList/deadlineList.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { DeadlineComponent } from '../deadline/deadline.component'
+
+@Component({
+    selector: 'deadlineList',
+    templateUrl: `
+    `,
+})
+export class DeadlineListComponent { 
+
+}
