3df6cc52cb3d16a197b2d2f97f0b6891632250e6
Elliott Beach <elliott2.71828@gmail.com>
2016-11-19 19:59:15 -0600

    adding deadlines!

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent }  from './components/app/app.component';
+import { DeadlineComponent }  from './components/deadline/deadline.component';
 
 @NgModule({
   imports:      [ BrowserModule, NgbModule.forRoot() ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, DeadlineComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/components/deadline/deadline.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'deadline',
+    templateUrl: 'app/components/deadline/deadline.component.html',
+    styles: [`
+    :host {
+    	color: red;
+	}
+  `]
+})
+export class DeadlineComponent { }
